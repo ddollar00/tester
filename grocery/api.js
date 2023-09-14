38b22954b4c8c97f092feeecf7f78487cb802047
Damion  Dollar <dollardamion@gmail.com>
2023-09-14 16:09:56 -0400

    grocery api

--- grocery/api.js
+++ grocery/api.js
@@ -2,6 +2,24 @@
 const http = require("http");
 const url = require('url');
 const PORT = 3000;
+const { createLogger, transports, format } = require('winston');
+
+
+// create the logger
+const logger = createLogger({
+    level: 'info', // this will log only messages with the level 'info' and above
+    format: format.combine(
+        format.timestamp(),
+        format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new transports.Console(), // log to the console
+        new transports.File({ filename: 'info.log', level: 'info' }),
+
+    ]
+});
 const groc = [];
 const server = http.createServer((req, res) => {
 
@@ -10,6 +28,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
 
         res.end(JSON.stringify(groc));
+        logger.info(" Get " + JSON.stringify(groc));
         //POST
     } else if (req.method === 'POST' && req.url === '/api/add') {
         let body = '';
@@ -20,7 +39,7 @@ const server = http.createServer((req, res) => {
 
             const item = JSON.parse(body);
             groc.push(item);
-
+            logger.info(" ADD " + JSON.stringify(item));
             res.writeHead(201, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'item added Successfully!' }));
 
@@ -39,6 +58,7 @@ const server = http.createServer((req, res) => {
 
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'item bought status changed!' }));
+            logger.info(" Edit" + JSON.stringify(groc[Number(data) - 1]));
 
         });
     }
@@ -55,6 +75,7 @@ const server = http.createServer((req, res) => {
 
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'item has been deleted!' }));
+            logger.info(" Delete" + JSON.stringify(groc[Number(data) - 1]));
 
         });
     } else {
